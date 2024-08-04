68c86aad5e72224132258da260bc2e22ab74c2b7
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-08-04 14:56:32 +0530

    Change active class on arrow click

--- src/js/image-carousel.js
+++ src/js/image-carousel.js
@@ -8,8 +8,12 @@ import background4 from "../assets/images/bg4.jpg";
 class ImageCarousel {
 	constructor() {
 		this.carouselImagesEle = document.querySelector(".carousel-images");
+		this.leftArrow = document.querySelector(".left-arrow");
+		this.rightArrow = document.querySelector(".right-arrow");
+		this.activeIndex = 2;
 		this.images = [background1, background2, background3, background4];
 		this.loadImages();
+		this.bindEvents();
 	}
 
 	loadImages() {
@@ -17,10 +21,34 @@ class ImageCarousel {
 			const imageEle = document.createElement("img");
 			imageEle.setAttribute("src", image);
 			imageEle.classList.add("carousel-image");
-			if (index == 2) imageEle.classList.add("active");
+			if (index == this.activeIndex) imageEle.classList.add("active");
 			this.carouselImagesEle.appendChild(imageEle);
 		});
 	}
+	bindEvents() {
+		this.leftArrow.addEventListener("click", () => this.shiftCarousel(-1));
+		this.rightArrow.addEventListener("click", () => this.shiftCarousel(1));
+	}
+
+	shiftCarousel = (value) => {
+		const newIndex = this.activeIndex + value;
+		if (newIndex >= 0 && newIndex < this.images.length) {
+			this.updateActiveIndex(newIndex);
+		}
+	};
+
+	getActiveImages = () =>
+		Array.from(document.querySelectorAll(".carousel-image"));
+
+	updateActiveIndex = (value) => {
+		this.activeIndex = value;
+		const imagesEle = this.getActiveImages();
+		imagesEle.forEach((imageEle, index) => {
+			console.log("HEY");
+			if (index === value) imagesEle[index].classList.add("active");
+			else imagesEle[index].classList.remove("active");
+		});
+	};
 }
 
 export default new ImageCarousel();
